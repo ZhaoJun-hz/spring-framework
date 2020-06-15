11711f987127594c655136c385fc595b63eb5c13
ZhaoJun-hz <zhaojuncodeing@163.com>
2020-06-15 16:24:22 +0800

    循环依赖

---
+++ spring-mydemo/src/main/java/com/zj/pojo/circle/TestC.java
@@ -0,0 +1,21 @@
+package com.zj.pojo.circle;
+
+public class TestC {
+	private TestA testA;
+
+	public void c() {
+		testA.a();
+	}
+
+	public TestA getTestA() {
+		return testA;
+	}
+
+	public void setTestA(TestA testA) {
+		this.testA = testA;
+	}
+
+	// public TestC(TestA testA) {
+	// 	this.testA = testA;
+	// }
+}
